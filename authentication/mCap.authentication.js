f7064139554bc789d096b03840cfc382a6aaa3cb
hano <its@time2dev.com>
2014-05-14 17:09:52 +0200

    add authentication example

--- authentication/mCap.authentication.js
+++ authentication/mCap.authentication.js
@@ -4,21 +4,44 @@
   var Authentication = mCap.Model.extend({
 
     defaults: {
-      user: null
+      "username": "",
+      "organization": "",
+      "password": ""
     },
 
-    endpoint: 'gofer/security/rest/login/',
+    endpoint: 'gofer/security/rest/auth/',
 
-    login: function () {
-      return this.save().then(function(){
+    login: function (options) {
+      if (typeof options === 'string') {
+        this.set('password', options);
+      } else if (typeof options === 'object') {
+        this.set(options);
+      }
 
-      }).fail(function(){
-
+      return this.sync('create', this, {
+        url: this.url() + 'login'
+      }).always(function () {
+        if (typeof options === 'string') {
+          this.set('password', '');
+        }
       });
     },
 
     logout: function () {
+      return this.sync('create', this, {
+        url: this.url() + 'logout'
+      }).always(function () {
+      });
+    },
 
+    parse: function (data) {
+      var attributes = {};
+      if (data) {
+        if (data.user) {
+          attributes.user = new mCap.User(data.user);
+        }
+      }
+      return attributes;
     }
 
   });
