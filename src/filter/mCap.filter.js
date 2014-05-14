7736a16223aa00696da288891c2c47ae81393235
zarlex <alex.zarges@online.de>
2014-05-14 17:25:52 +0200

    created src folder

---
+++ src/filter/mCap.filter.js
@@ -0,0 +1,59 @@
+var Filter = function () {
+  // If it is an invalid value return null otherwise the provided object
+  var returnNullOrObjectFor = function (value, object) {
+    return (!angular.isDefined(value) || value === null || value === '') ? null : object;
+  };
+
+  // See https://wiki.mwaysolutions.com/confluence/display/mCAPTECH/mCAP+REST+API#mCAPRESTAPI-Filter
+  // for more information about mcap filter api
+  return {
+    containsString: function (fieldName, value) {
+      return returnNullOrObjectFor(value, {
+        type: 'containsString',
+        fieldName: fieldName,
+        contains: value
+      });
+    },
+
+    and: function (filters) {
+      return this.logOp(filters, 'AND');
+    },
+
+    nand: function (filters) {
+      return this.logOp(filters, 'NAND');
+    },
+
+    or: function (filters) {
+      return this.logOp(filters, 'OR');
+    },
+
+    logOp: function (filters, operator) {
+      filters = _.without(filters, null); // Removing null values from existing filters
+
+      return filters.length === 0 ? null : { // Ignore logOps with empty filters
+        type: 'logOp',
+        operation: operator,
+        filters: filters
+      };
+    },
+
+    boolean: function (fieldName, value) {
+      return returnNullOrObjectFor(value, {
+        type: 'boolean',
+        fieldName: fieldName,
+        value: value
+      });
+    },
+
+    like: function (fieldName, value) {
+      return returnNullOrObjectFor(value, {
+        type: 'like',
+        fieldName: fieldName,
+        like: value
+      });
+    }
+  };
+
+};
+
+mCap.Filter = Filter;
