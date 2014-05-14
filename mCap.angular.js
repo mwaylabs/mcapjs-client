9a16fec59200364f70a0871187f9b40d44204bb1
hano <its@time2dev.com>
2014-05-14 13:52:45 +0200

    reformat code

--- mCap.angular.js
+++ mCap.angular.js
@@ -8,14 +8,14 @@
 
     .run(function ($http) {
 
-      if(!window.mCap){
+      if (!window.mCap) {
         throw new Error('Please include Mcap libary');
       }
 
       var sync = Backbone.sync;
 
-      Backbone.sync = function(method, model){
-        return sync.apply(Backbone,arguments).then(function(){
+      Backbone.sync = function (method, model) {
+        return sync.apply(Backbone, arguments).then(function () {
           return model;
         });
       };
@@ -23,7 +23,7 @@
       Backbone.ajax = function (options) {
 
         // Ignore notifications for given response codes
-        if(options.data) {
+        if (options.data) {
           var requestData = JSON.parse(options.data);
           options.ignoreHandleResponseCodes = requestData.ignoreHandleResponseCodes;
         }
@@ -44,23 +44,23 @@
           mCap.application.setBaseUrl(url);
         },
 
-        $get: function(){
+        $get: function () {
           return window.mCap.application;
         }
       };
     })
 
-    .service('mCap',function(){
+    .service('mCap', function () {
       return window.mCap;
     })
 
-    .service('mCapCollection',function(mCap){
+    .service('mCapCollection', function (mCap) {
       return mCap.Collection;
     })
 
-    .service('mCapModel',function($rootScope, mCap){
+    .service('mCapModel', function ($rootScope, mCap) {
       return mCap.Model.extend({
-        set: function(){
+        set: function () {
           // Trigger digest cycle to make calls to set recognizable by angular
           if (!$rootScope.$$phase) {
             $rootScope.$apply();
