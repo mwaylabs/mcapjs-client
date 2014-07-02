22cb380bd59e906458ec6846d250163023dd8be8
hano <its@time2dev.com>
2014-07-02 12:42:10 +0200

    add comments to examples

--- examples/authentication/isAuthenticated/main.js
+++ examples/authentication/isAuthenticated/main.js
@@ -1,24 +1,35 @@
-var BASE_URL  = 'https://example.com:8443/';
-var USER = 'username';
-var PASSWORD = 'password';
-
+// the server url
+var BASE_URL  = 'https://coredev4.mwaysolutions.com:443/';
+// the username
+var USER = 'mhano';
+// the password
+var PASSWORD = 'mhano1';
+// first configure the base URL
 mCAP.application.set('baseUrl', BASE_URL);
+// then configure the authentication
 mCAP.authentication.set('userName', USER);
+// you don't have to store the password. It is also possible to give it as parameter to the login
 mCAP.authentication.set('password', PASSWORD);
 
+// perform a login
 mCAP.authentication.login().then(function(){
+  // after succ login perform a logout
     return mCAP.authentication.logout();
 }).then(function(){
+ // check the authentication state
     return mCAP.authentication.isAuthenticated();
 }).fail(function(){
-    console.log('is not authenticated');
+  // login failed
+    log('is not authenticated');
+  // login again
     mCAP.authentication.login().then(function(){
+      // check the authentication state
         return mCAP.authentication.isAuthenticated().then(function(){
-            console.log('is authenticated');
+            log('is authenticated');
         }).fail(function(){
-            console.log('is not authenticated');
+            log('is not authenticated');
         });
     }).fail(function(){
-        console.log('login err');
+        log('login err');
     });
 });
