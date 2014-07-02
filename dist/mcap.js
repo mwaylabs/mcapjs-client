1067f1d643226caa4614d2c269adc7ff6b71a60d
hano <its@time2dev.com>
2014-07-02 10:55:14 +0200

    add dist with login/logout events

--- dist/mcap.js
+++ dist/mcap.js
@@ -16,6 +16,46 @@
     return sync.apply(Backbone, [method, model, options]);
   };
 
+  //INCLUDE GLOBAL SETTINGS HERE
+  /**
+   * Utils namespace
+   * @type {Object}
+   */
+  mCAP.Utils = {};
+  /**
+   * Send a request to with the given settings
+   * @param settings
+   * url: request URL
+   * dataType: the datatype of the request - default is empty string
+   * data: the data to send with the request - default is empty string
+   * method: the method of the request e.g. PUT/POST/GET/DELETE
+   * contentType: the contentType header
+   * timeout: timeout until request expires
+   * @returns {*}
+   */
+  mCAP.Utils.request = function (settings) {
+  
+    var ajaxOptions = {
+      url: settings.url,
+      dataType: settings.dataType || '',
+      data: settings.data || {t: new Date().getTime()}
+    };
+  
+    if (settings.method) {
+      ajaxOptions.method = settings.method;
+    }
+  
+    if (settings.contentType) {
+      ajaxOptions.contentType = settings.contentType;
+    }
+  
+    if (settings.timeout) {
+      ajaxOptions.timeout = settings.timeout;
+    }
+  
+    return $.ajax(ajaxOptions);
+  };
+
   // INCLUDE PRIVATE VARS HERE
   /*jshint unused:false */
   var Filterable = function (collectionInstance, options) {
@@ -661,21 +701,26 @@
       } else if (typeof options === 'object') {
         this.set(options);
       }
-  
       return this.save(null, {
         url: this.url() + 'login'
       }).always(function () {
         if (typeof options === 'string') {
           that.set('password', '');
         }
+        var args = Array.prototype.slice.call(arguments, 0);
+        args.unshift('login');
+        that.trigger.apply(that, args);
       });
     },
   
     logout: function () {
+      var that = this;
       return this.save(null, {
         url: this.url() + 'logout'
       }).always(function () {
-  
+        var args = Array.prototype.slice.call(arguments, 0);
+        args.unshift('logout');
+        that.trigger.apply(that, args);
       });
     },
   
@@ -690,6 +735,43 @@
         }
       }
       return attributes;
+    },
+  
+    /**
+     * Check if the current user is authenticated or not. Resolves if it is the case otherwise its rejected.
+     * @returns {promise}
+     * @example
+     * mCAP.authentication.isAuthenticated().then(function(){
+              console.log('is authenticated');
+          }).fail(function(){
+              console.log('is not authenticated');
+          });
+     */
+    isAuthenticated: function () {
+      var dfd = $.Deferred();
+      var uuid = null;
+      // check if there was a login before
+      if(mCAP.authentication.get('user') && mCAP.authentication.get('user').get('uuid')){
+        uuid = mCAP.authentication.get('user').get('uuid');
+      } else {
+        dfd.reject('no user set');
+        return dfd.promise();
+      }
+  
+      mCAP.Utils.request({
+        url: mCAP.application.get('baseUrl') + 'gofer/system/security/currentAuthorization'
+      }).then(function (data) {
+        // resolve only if the current user is authenticated
+        if (data.user && data.user.uuid && data.user.uuid === uuid) {
+          dfd.resolve(data);
+        }
+        // otherwise reject
+        dfd.reject('not authenticated', data);
+        return;
+      }).fail(function (err) {
+        dfd.reject(err);
+      });
+      return dfd.promise();
     }
   
   });
