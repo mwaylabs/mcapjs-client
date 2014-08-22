37a5918858cb236fa532c81940a624294dd16d81
zarlex <alex.zarges@online.de>
2014-08-22 17:24:34 +0200

    added replace function which throws a replace event (in case the whole colelction is swapped with a selection) and overwrote the backbone _prepare model function to make the call of prepare of its model possible

--- src/collection/collection.js
+++ src/collection/collection.js
@@ -47,6 +47,54 @@ var Collection = Backbone.Collection.extend({
     }
     options = mCAP.Utils.setAuthenticationEvent(options);
     return Backbone.Collection.prototype.sync.apply(this, [method, model, options]);
+  },
+
+  replace: function(models){
+    this.reset(models);
+    this.trigger('replace',this);
+  },
+
+  /*
+   * README: Why are we doing this??
+   *
+   * The initialize method of a  model is not always called at the same time
+   * When the model instance is created by the collection the parse method is called before
+   * For referenced collections/models we need to set them up before the parse method is called
+   * We Can not do this in the attributes field because the default attributes are set after the parse method
+   * The only solution is that we have a function that comes always before the parse method and that is also
+   * called when a model instance is created manually(not by a collection)
+   * The following code get the prototype function of the original parse method which is then overwritten by our
+   * custom 'prepare' function. After the prepared has been called we call the original parse method
+   *
+   * New Execution order of functions:
+   * create new Model(): prepare() > initialize()
+   * Colellection creates model instance: prepare() > parse() > initialize()
+   */
+
+  _prepareModel: function(model,options){
+    var orgParse = this.model.prototype.parse,
+        collection = this,
+        preparedAttrs = {},
+        parsedAttrs = {};
+
+    //if(model.uuid ==='2345423543252')debugger;
+
+    //Inject the function which should be called before the parse method into the original parse method
+    if(options.parse){
+      this.model.prototype.parse = function(){
+        if(!this._prepared){
+          preparedAttrs = collection.model.prototype.prepare.apply(this,arguments);
+          this.set(preparedAttrs);
+          this._prepared = true;
+        }
+        this.parse = orgParse;
+        parsedAttrs = this.parse.apply(this,arguments);
+        //Merge the attributes which are then set as model attributes
+        return _.extend(parsedAttrs,preparedAttrs);
+      };
+    }
+
+    return Backbone.Collection.prototype._prepareModel.apply(this,arguments);
   }
 
 });
