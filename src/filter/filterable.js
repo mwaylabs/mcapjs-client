be556b416e4002def48185f45c5128904eebb32d
pwambach <philippwambach@googlemail.com>
2014-10-20 16:16:41 +0200

    made customUrlParams resettable in filterable

--- src/filter/filterable.js
+++ src/filter/filterable.js
@@ -9,13 +9,14 @@ var Filterable = function (collectionInstance, options) {
       _page = options.page || 1,
       _perPage = options.perPage || 30,
       _initialFilterValues = options.filterValues ? JSON.parse(JSON.stringify(options.filterValues)) : options.filterValues,
+      _initialCustomUrlParams = _.clone(options.customUrlParams),
       _filterDefinition = options.filterDefinition,
       _sortOrder = options.sortOrder,
       _totalAmount,
       _lastFilter;
 
-  this.filterValues = options.filterValues || [];
-  this.customUrlParams = options.customUrlParams;
+  this.filterValues = options.filterValues || {};
+  this.customUrlParams = options.customUrlParams || {};
 
   this.getRequestParams = function (method, model, options) {
     options.params = options.params || {};
@@ -125,6 +126,7 @@ var Filterable = function (collectionInstance, options) {
 
   this.resetFilters = function () {
     this.filterValues = _initialFilterValues ? JSON.parse(JSON.stringify(_initialFilterValues)) : _initialFilterValues;
+    this.customUrlParams = _initialCustomUrlParams;
   };
 
   (function _main() {
