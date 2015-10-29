0febbba9380aefe0ccf0fe3018d5a4007b174c00
zarlex <alex.zarges@online.de>
2015-10-29 18:48:53 +0100

    adds functionality to persist filter values and customUrlParams

--- src/filter/filter_holder_model.js
+++ src/filter/filter_holder_model.js
@@ -5,50 +5,83 @@ var FilterHolder = mCAP.Model.extend({
 
   endpoint: 'gofer/filter/rest/filterHolders',
 
-  defaults: function(){
+  defaults: function () {
     return {
       aclEntries: [],
       filter: null,
       group: '',
       name: '',
       version: 0,
-      filterValues:{}
+      filterValues: {},
+      customUrlParams: {}
     };
   },
 
-  parse: function(){
-    var parsed = mCAP.Model.prototype.parse.apply(this,arguments),
-        data = parsed.data || parsed,
-        split = data.name.split('#VAL');
+  _parseFilterToValuesAndUrlParams: function (filter, result) {
+    result = result || {};
+    if (filter.type === 'logOp') {
+      filter.filters.forEach(function (filter) {
+        this._parseFilterToValuesAndUrlParams(filter, result);
+      }, this);
+      return result;
+    } else {
+      if (filter.fieldName === '__filterValues__') {
+        result.filterValues = JSON.parse(filter.value);
+      }
+      if (filter.fieldName === '__customUrlParams__') {
+        result.customUrlParams = JSON.parse(filter.value);
+      }
+      return result;
+    }
+  },
 
-    data.name = split[0];
-    if(split[1]){
-      data.filterValues = JSON.parse(split[1]);
+  parse: function () {
+    var parsed = mCAP.Model.prototype.parse.apply(this, arguments),
+        data = parsed.data || parsed,
+        result = this._parseFilterToValuesAndUrlParams(data.filter, {});
+
+    if (result.filterValues) {
+      data.filterValues = result.filterValues;
+    }
+    if (result.customUrlParams) {
+      data.customUrlParams = result.customUrlParams;
     }
 
     return data;
   },
 
-  beforeSave: function(attrs){
+  beforeSave: function (attrs) {
     var currentUserUuid = mCAP.authenticatedUser.get('uuid');
-    if(currentUserUuid){
-      attrs.aclEntries.push(currentUserUuid+':rw');
+    if (currentUserUuid) {
+      attrs.aclEntries.push(currentUserUuid + ':rw');
     }
     attrs.version += 1;
-    if(_.size(attrs.filterValues)>0){
-      attrs.name = attrs.name+'#VAL'+JSON.stringify(attrs.filterValues);
-    }
-    delete attrs.totalAmount;
 
+    var filters = [];
+    if (_.size(attrs.filterValues) > 0) {
+      var filterValuesFilter = (new mCAP.Filter()).string('__filterValues__', JSON.stringify(attrs.filterValues));
+      filters.push(filterValuesFilter);
+    }
+    if (_.size(attrs.customUrlParams) > 0) {
+      var customUrlParamsFilter = (new mCAP.Filter()).string('__customUrlParams__', JSON.stringify(attrs.customUrlParams));
+      filters.push(customUrlParamsFilter);
+    }
+    if (filters.length > 0) {
+      attrs.filter = (new mCAP.Filter()).or(filters);
+    }
+
+    delete attrs.totalAmount;
     delete attrs.filterValues;
+    delete attrs.customUrlParams;
     return attrs;
   },
 
-  isValid: function(){
+  isValid: function () {
     var name = this.get('name'),
-        filterValues = this.get('filterValues');
+      filterValues = this.get('filterValues'),
+      urlParams = this.get('customUrlParams');
 
-    return (name && name.length>0 && filterValues && _.size(filterValues)>0);
+    return (name && name.length > 0 && ( (filterValues && _.size(filterValues) > 0) || (urlParams && _.size(urlParams) > 0) ) );
   }
 
 });
