360fee06419a04c1bcd48026c769c3ca33d77b12
zarlex <alex.zarges@online.de>
2016-02-03 11:20:56 +0100

    adds registration functionality

--- src/security/authentication.js
+++ src/security/authentication.js
@@ -138,6 +138,24 @@ var Authentication = mCAP.Model.extend({
       mCAP.authentication.set(response);
       return mCAP.authentication;
     });
+  },
+  canRegister: function(){
+    return mCAP.Utils.request({
+      url: mCAP.Utils.getUrl('/relution/api/v1/registration/available'),
+      type: 'GET'
+    }).then(function (response) {
+      return response.status === 200;
+    }, function(){
+      return false;
+    });
+  },
+  register: function(registrationObj){
+    registrationObj = registrationObj || {};
+    return mCAP.Utils.request({
+      url: mCAP.Utils.getUrl('/relution/api/v1/registration'),
+      type: 'POST',
+      data: registrationObj
+    });
   }
 });
 
