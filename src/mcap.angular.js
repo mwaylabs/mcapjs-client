3c23e67bcea47751c709f2775127be75ebe24ecf
Zarlex <alex.zarges@online.de>
2016-08-10 15:34:14 +0200

    removes backbone angular adapter

--- src/mcap.angular.js
+++ src/mcap.angular.js
@@ -1,69 +1,27 @@
-(function (root, angular, Backbone) {
+(function (root, angular) {
 
   'use strict';
 
-  (function () {
-    var _$q;
-    var _$http;
-    var _sync = Backbone.sync;
+  angular.module('mCAP', [])
 
-    angular.module('mCAP', [])
+    .service('MCAPFilterHolder', function () {
+      return window.mCAP.FilterHolder;
+    })
 
-      .service('MCAPFilterHolder', function () {
-        return window.mCAP.FilterHolder;
-      })
+    .service('MCAPFilterHolders', function () {
+      return window.mCAP.FilterHolders;
+    })
 
-      .service('MCAPFilterHolders', function () {
-        return window.mCAP.FilterHolders;
-      })
+    .service('MCAPCollection', function () {
+      return window.mCAP.Collection;
+    })
 
-      .service('MCAPCollection', function () {
-        return window.mCAP.Collection;
-      })
+    .service('MCAPEnumerableCollection', function () {
+      return window.mCAP.EnumerableCollection;
+    })
 
-      .service('MCAPEnumerableCollection', function () {
-        return window.mCAP.EnumerableCollection;
-      })
+    .service('MCAPModel', function () {
+      return window.mCAP.Model;
+    });
 
-      .service('MCAPModel', function () {
-        return window.mCAP.Model;
-      })
-
-      .run(function ($http, $q) {
-        _$q = $q;
-        _$http = $http;
-      });
-
-    Backbone.sync = function (method, model, options) {
-      var dfd = _$q.defer();
-      _sync.apply(Backbone, [method, model, options]).then(function () {
-        dfd.resolve(model);
-      }, function (resp) {
-        dfd.reject(resp);
-      });
-      return dfd.promise;
-    };
-
-    Backbone.ajax = function (options) {
-      // Set HTTP Verb as 'method'
-      options.method = options.type;
-
-      var dfd = _$q.defer();
-      // Use angulars $http implementation for requests
-      _$http.apply(angular, [options]).then(function (resp) {
-        if (options.success && typeof options.success === 'function') {
-          options.success(resp);
-        }
-        dfd.resolve(resp);
-      }, function (resp) {
-        if (options.error && typeof options.error === 'function') {
-          options.error(resp);
-        }
-        dfd.reject(resp);
-      });
-      return dfd.promise;
-    };
-
-  }());
-
-})(window, angular, Backbone);
+})(window, angular);
